bfc36ddf988ed984404aea3e9ef7443d4089bebd
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-12 18:50:04 +0430

    add lint command and fix code

--- src/app.js
+++ src/app.js
@@ -7,34 +7,34 @@ const mongoose = require("mongoose")
 const morgan = require("morgan")
 
 module.exports = class App {
-    constructor() {
-        this.app = express()
-    }
+	constructor() {
+		this.app = express()
+	}
 
-    /** Run all methods
+	/** Run all methods
 	 * @public
 	 */
 	initialize() {
-        this.setupExpress()
-        this.setupMongodb()
-        this.configuration()
+		this.setupExpress()
+		this.setupMongodb()
+		this.configuration()
 	}
 
-    /** Setup server with express
+	/** Setup server with express
 	 * @private
 	 * @package http, express
 	 */
-    setupExpress() {
-        const server = createServer(this.app)
-        server.listen(3000, () =>  console.log("Server running on port 3000"))
-    }
+	setupExpress() {
+		const server = createServer(this.app)
+		server.listen(3000, () =>  console.log("Server running on port 3000"))
+	}
 
-    /** Setup mongodb and set config
+	/** Setup mongodb and set config
 	 * @private
 	 * @package mongoose
 	 */
-    setupMongodb() {
-        mongoose.Promise = global.Promise
+	setupMongodb() {
+		mongoose.Promise = global.Promise
 		mongoose.connect("", {
 			useCreateIndex: true,
 			useFindAndModify: false,
@@ -44,23 +44,23 @@ module.exports = class App {
 		err => {
 			err ? console.log(err.message) : console.log("Database connected")
 		})
-    }
+	}
 
-    /** Setup and using packages
+	/** Setup and using packages
 	 * @private
 	 * @package helmet, cors, body-parser, contentType, morgan
 	 */
-    configuration() {
-        this.app.use(helmet())
-        this.app.use(cors({
+	configuration() {
+		this.app.use(helmet())
+		this.app.use(cors({
 			credentials: true,
 			methods: "GET, POST, PUT, DELETE",
 			origin: "*"
-        }))
-        this.app.use(json())
+		}))
+		this.app.use(json())
 		this.app.use(urlencoded({
 			extended: true
-        }))
-        this.app.use(morgan("dev"))
-    }
+		}))
+		this.app.use(morgan("dev"))
+	}
 }
