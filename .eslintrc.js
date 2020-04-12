57c8695f4c618640d839e2f600a165476e238eb6
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-12 18:49:24 +0430

    install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	"env": {
+		"commonjs": true,
+		"es6": true,
+		"node": true
+	},
+	"extends": "eslint:recommended",
+	"globals": {
+		"config": "readonly"
+	},
+	"parserOptions": {
+		"ecmaVersion": 2018,
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+		"sort-keys": "error",
+		"no-useless-catch": 0
+	}
+}
