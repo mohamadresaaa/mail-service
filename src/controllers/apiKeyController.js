1b61c62722b4862d0e6da19575132e992abf0148
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-13 13:22:45 +0430

    add apiKeyController

---
+++ src/controllers/apiKeyController.js
@@ -0,0 +1,35 @@
+const baseController = require("./baseController")
+
+module.exports = new class ApiKeyController extends baseController {
+	async list(req, res, next) {
+		try {
+			res.send("list of apiKeys")
+		} catch (error) {
+			next(error)
+		}
+	}
+
+	async create (req, res, next) {
+		try {
+			res.send("create new apiKey")
+		} catch (error) {
+			next(error)
+		}
+	}
+    
+	async update(req, res, next) {
+		try {
+			res.send("update apiKey")
+		} catch (error) {
+			next(error)
+		}
+	}
+    
+	async remove(req, res, next) {
+		try {
+			res.send("remove apiKey")
+		} catch (error) {
+			next(error)
+		}
+	}
+}
