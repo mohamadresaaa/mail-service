e15408ac5c1b9bd4428f58e2877ad28a08acd6f3
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-13 19:43:56 +0430

    add create credential

---
+++ src/services/credential/create.js
@@ -0,0 +1,25 @@
+/** Create a new credential and generate apiKey
+ * @param {object} controller
+ * @param {string} name from req.bod
+ * @param {string} user from req.body
+ * @param {object} res from express
+ * @returns {response} message
+ */
+module.exports = async (controller, data, res) => {
+	try {
+		// Credential model
+		const { Credential } = controller[Symbol.for("models")]
+
+		// Create new apiKey and generate it
+		let credential =  await new Credential({
+			...data
+		}).save()
+
+		// Return message
+		return controller.infoMessage(res, 200, {
+			apiKey: credential.apiKey
+		})
+	} catch (error) {
+		throw error
+	}
+}
